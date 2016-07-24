4d6d4a8de19c8d05af77afa1eedd8723e8c12d35
Vu Tran <vu@vu-tran.com>
2016-07-24 16:40:58 -0700

    added cache layer

--- index.js
+++ index.js
@@ -40,40 +40,63 @@ const iconDir = path.resolve(__dirname, 'imageCache');
  */
 const iconPath = id => path.resolve(iconDir, id.toString() + '.png');
 
-alfy.fetch(lookup(alfy.input))
-  .then(data => {
-    fs.mkdirp(iconDir, err => {
-      if (!err) {
-        // download the image
-        base64.encode(data.sprites.front_default, {}, (err, image) => {
-          // write the image to disk
-          fs.writeFile(iconPath(data.id), image);
-          const title = '#' + data.id + ': ' + capitalize(data.name);
-          // output items
-          const items = [
-            {
-              title: title,
-              subtitle: data.types.map(t => capitalize(t.type.name)).join(', '),
-              arg: openUrl(data.name),
-              icon: {
-                path: iconPath(data.id),
-              }
-            },
-          ];
-          alfy.output(items);
+// from cache
+const results = alfy.cache.get(alfy.input);
+
+/**
+ * Makes a new request to the API for data
+ *
+ * @param {String} input
+ * @return {Promise}
+ */
+const makeRequest = input => {
+  return new Promise(resolve => {
+    alfy.fetch(lookup(alfy.input))
+      .then(data => {
+        fs.mkdirp(iconDir, err => {
+          if (!err) {
+            // download the image
+            base64.encode(data.sprites.front_default, {}, (err, image) => {
+              // write the image to disk
+              fs.writeFile(iconPath(data.id), image);
+              const title = '#' + data.id + ': ' + capitalize(data.name);
+              // output items
+              const items = [
+                {
+                  title: title,
+                  subtitle: data.types.map(t => capitalize(t.type.name)).join(', '),
+                  arg: openUrl(data.name),
+                  icon: {
+                    path: iconPath(data.id),
+                  }
+                },
+              ];
+              alfy.output(items);
+              resolve(items);
+            });
+          }
         });
-      }
-    });
-  })
-  .catch(err => {
-  	console.log(JSON.stringify({
-  		items: [{
-  			title: err.name,
-  			subtitle: err.message,
-  			valid: false,
-  			text: {
-  				copy: err.stack
-  			}
-  		}]
-  	}));
+      })
+      .catch(err => {
+        console.log(JSON.stringify({
+          items: [{
+            title: err.name,
+            subtitle: err.message,
+            valid: false,
+            text: {
+              copy: err.stack
+            }
+          }]
+        }));
+      });
   });
+}
+
+if (!results) {
+  makeRequest(alfy.input)
+    .then(res => {
+      alfy.cache.set(alfy.input, res);
+    });
+} else {
+  alfy.output(results);
+}
